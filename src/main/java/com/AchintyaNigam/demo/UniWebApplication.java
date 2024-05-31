9025a2b7633a027eb07692d99e6e2473ac565de6
AchintyaNigam <achintyanigam2@gmail.com>
2024-05-31 08:49:41 +0530

    db access password encrypted

--- src/main/java/com/AchintyaNigam/demo/UniWebApplication.java
+++ src/main/java/com/AchintyaNigam/demo/UniWebApplication.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 
 @SpringBootApplication
 @EnableCaching  
+@EnableEncryptableProperties
 public class UniWebApplication {
 
 	public static void main(String[] args) {
